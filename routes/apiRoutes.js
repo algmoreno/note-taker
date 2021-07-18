0cc32c9c7cea77382143f147478be50dad58cf1f
algmoreno <alg.moreno00@gmail.com>
2021-07-18 15:34:46 -0700

    all routes working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,42 +1,47 @@
 const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
-const UUID = require('uuid');
+var UUID = require('uuid');
+
+let notes = require('../db/db')
 
 // API Routes
 router.get('/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        let dbNotes = JSON.parse(data);
-        res.send(dbNotes);
-        if (err) {
-            return;
-        }
-    });
+    // fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    //     let dbNotes = JSON.parse(data);
+    //     res.send(dbNotes);
+    //     if (err) {
+    //         return;
+    //     }
+    // });
+    res.json(notes);
 });
 
 router.post('/notes', (req, res) => {
+    let request = req.body;
     req.body.id = UUID.v1();
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        let dbNotes = JSON.parse(data);
-        // let note = req.body;
-        dbNotes.push(req.body);
-        // updateDbFile(note); 
-    fs.writeFile('./db/db.json', JSON.stringify(dbNotes), (err) => {
-            if (err) {
-                console.log("error")
-            }
-            res.json(req.body);
-        })
-    });
+    console.log(req.body.id);
+    notes.push(request);
+    res.json(notes);
+
+    fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(notes))
+    
 });
 
-router.delete('/notes/:id')
-// router.get('/notes/:id', (req, res) => {
-//     fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
-//         const dbNotes = JSON.parse(data);
-//     res.send(dbNotes[req.params.id])
-//     })
-// });
+router.delete('/notes/:id', (req, res) => {
+    const { id } = req.params;
+    console.log(req.params.id);
+    let deletedId = notes.find(notes => notes.id === id); 
+    if (deletedId) {
+      notes = notes.filter(notes => notes.id !== id)
+      res.end();
+      res.status(200)
+    }
+    else {
+      res.status(404)
+    }
+})
+
 
 
 module.exports = router;
